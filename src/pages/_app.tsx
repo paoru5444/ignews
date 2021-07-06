c847f0ff0f3af7e4464ecad77c0f30e738c79277
Paulo Roberto <betinho5444@gmail.com>
2021-07-06 08:33:46 -0300

    feat: api routes and faunadb

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { Header } from '../components/Header';
+import '../styles/global.scss';
+import { Provider as NextAuthProvider } from 'next-auth/client';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextAuthProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </NextAuthProvider>
+  )
+}
+
+export default MyApp
