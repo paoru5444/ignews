0eda853ac6e51629221429847689161429b0e350
Paulo Roberto <betinho5444@gmail.com>
2021-10-01 08:59:02 -0300

    feat: stripe config, payment and webhooks

---
+++ notes.md
@@ -0,0 +1,9 @@
+# IgNews notes
+
+### 3 lugares no next para fazer operações de forma segura:
+- getServerSideProps (SSR)
+- getStaticProps (SSG)
+- API routes
+
+### Webhook
+- É um partner para integração de sistemas na web
--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { session, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJS } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +8,30 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  async function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJS()
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message)
+    }
+  }
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
