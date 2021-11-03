31502ae6126b09112afeaef355410b56497c300c
Paulo Roberto <paulo.pereira.dev@gmail.com>
2021-11-03 09:45:03 -0300

    feat: crate active link component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
+import { ActiveLink } from '../../components/ActiveLink'
+
 export function Header() {
 
   return (
@@ -8,8 +10,12 @@ export function Header() {
         <div className={styles.headerContent}>
           <img src="/images/logo.svg" alt="ig.news" />
           <nav>
-            <a href="" className={styles.active}>Home</a>
-            <a href="/posts">Posts</a>
+            <ActiveLink href="/" activeClassName={styles.active}>
+              <a  >Home</a>
+            </ActiveLink>
+            <ActiveLink href="/posts" prefetch activeClassName={styles.active}>
+              <a>Posts</a>
+            </ActiveLink>
           </nav> 
           <SignInButton />
         </div>
